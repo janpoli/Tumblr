3e44d1dab5e36443c017370e05dd85166de24ec0
janpoli <jansocorro@gmail.com>
2017-09-13 13:24:01 +0800

    Added a line of code in application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
